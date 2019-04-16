2b0ec403c425a3081de775dfc206ea1cbd666233
Cristian Bullokles <cristian.bullokles@fukuroo.io>
2019-04-16 00:26:22 -0300

    Files

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection ({
+    host: '10.109.48.5',
+    user: 'apps',
+    password: 'sils123',
+    database: 'sils'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+
+app.get('/', (req, res) => {
+  console.log('Hello world received a request.');
+  db.query('SELECT * FROM Estados LIMIT 5', (err, result) => {
+    if (err) {
+      console.log('ERR')
+      return res.status(500).send(err);
+    }
+    else{
+      console.log('OK')
+      return res.status(200).send(result);
+    }
+  })
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
